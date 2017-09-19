389fff05b9735fdaf495b9666ea3e1dad90d7a82
Atul Parate <Atul.Parate@synechron.com>
2017-09-19 19:34:33 +0530

    for single feature run

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // `wdio` will be called from there.
     //
     specs: [
-        './src/features/**/*.feature',
+        './src/features/**/newLeaveApplication.feature',
     ],
     // Patterns to exclude.
     exclude: [
