ad303e868387e6fd8d98c06d10484e1dc0a7eb4b
Atul Parate <Atul.Parate@synechron.com>
2017-09-21 19:56:48 +0530

    ok

--- src/support/action/getValue.js
+++ src/support/action/getValue.js
@@ -1,3 +1,17 @@
-module.exports = function( name ) {
-    console.log("Print Variable ", global[name]);
+/**
+ * Open the given URL
+ * @param  {String}   type Type of navigation (url or site)
+ * @param  {String}   page The URL to navigate to
+ */
+module.exports = (name) => {
+    /**
+     * The URL to navigate to
+     * @type {String}
+     */
+   console.log("Print Variable ", name);
+      console.log("Print arguments ", arguments);
+
+   console.log("Print Variable ", global[name]);
+
+
 };
