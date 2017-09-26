5a179144ba8df0958a164a758a1f7734fab0327e
Atul Parate <Atul.Parate@synechron.com>
2017-09-26 17:37:19 +0530

    POM removed

--- src/support/objectRepository/loginPage.js
+++ src/support/objectRepository/loginPage.js
@@ -54,7 +54,10 @@ module.exports = () => {
    global.payeeAccountTextbox = "input[name='payeeaccount']";
 
 
-   
+   global.MiniStateTransactionTypeForWithdrawal = ".//*[@id='ministmt']/tbody/tr[3]/td[3]";
+   global.MiniStateTransactionTypeForDeposit = ".//*[@id='ministmt']/tbody/tr[2]/td[3]";
+   global.MiniStateAmountForWithdrawal = ".//*[@id='ministmt']/tbody/tr[3]/td[2]";
+   global.MiniStateAmountForDeposit = ".//*[@id='ministmt']/tbody/tr[2]/td[2]";
 
    global.Var_MiniStateTransactionTypeForWithdrawal = "w";
    global.var_MiniStateTransactionTypeForDeposit = "d";
@@ -62,11 +65,7 @@ module.exports = () => {
    global.var_MiniStateAmountForDeposit = "5000";
 
 
-    MiniStateTransactionTypeForWithdrawal: { get: function () { return browser.element('.//*[@id='ministmt']/tbody/tr[3]/td[3]'); } },
-    MiniStateTransactionTypeForDeposit: { get: function () { return browser.element('.//*[@id='ministmt']/tbody/tr[2]/td[3]'); } },
-    MiniStateAmountForWithdrawal:     { get: function () { return browser.element('.//*[@id='ministmt']/tbody/tr[3]/td[2]'); } },
-    MiniStateAmountForDeposit:    { get: function () { return browser.element('.//*[@id='ministmt']/tbody/tr[2]/td[2]'); } },
-
+    
 
 };
 
