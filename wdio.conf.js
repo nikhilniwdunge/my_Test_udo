79cd2178db3949c77fd433b2a426a2d3ce9fd23a
Atul Parate <Atul.Parate@synechron.com>
2017-09-28 13:00:09 +0530

    ignoreUndefinedDefinitions: true,

--- wdio.conf.js
+++ wdio.conf.js
@@ -143,7 +143,7 @@ exports.config = {
         failFast: false,
         // <boolean> Enable this config to treat undefined definitions as
         // warnings
-        ignoreUndefinedDefinitions: false,
+        ignoreUndefinedDefinitions: true,
         // <string[]> ("extension:module") require files with the given
         // EXTENSION after requiring MODULE (repeatable)
         name: [],
